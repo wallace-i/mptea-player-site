90c88e23279db8114fb16a2502cf06dedec5bf08
hitmon-gg <97249957+hitmon-gg@users.noreply.github.com>
2023-09-04 15:11:51 -0700

    - started Features
    ToDo:
    - Finish Features
    - develop each page
    - footer
    - header with graphics
    - more welcome page stuff?

--- src/main/java/dev/iandw/mpteaplayersite/views/features/FeaturesView.java
+++ src/main/java/dev/iandw/mpteaplayersite/views/features/FeaturesView.java
@@ -1,8 +1,8 @@
 package dev.iandw.mpteaplayersite.views.features;
 
-import com.vaadin.flow.component.html.H2;
-import com.vaadin.flow.component.html.Image;
-import com.vaadin.flow.component.html.Paragraph;
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.charts.model.Items;
+import com.vaadin.flow.component.html.*;
 import com.vaadin.flow.component.orderedlayout.FlexComponent;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
@@ -11,26 +11,28 @@ import com.vaadin.flow.theme.lumo.LumoUtility;
 import com.vaadin.flow.theme.lumo.LumoUtility.Margin;
 import dev.iandw.mpteaplayersite.views.MainLayout;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 @PageTitle("Features")
 @Route(value = "features", layout = MainLayout.class)
 public class FeaturesView extends VerticalLayout {
 
     public FeaturesView() {
-        setSpacing(false);
 
-        Image img = new Image("images/empty-plant.png", "placeholder plant");
-        img.setWidth("200px");
-        add(img);
+        H2 h2Features = new H2("Feature Summary");
+        Paragraph pFeature = new Paragraph();
+        pFeature.add("The following are key features of mpTea Player in a nutshell:");
 
-        H2 header = new H2("This place intentionally left empty");
-        header.addClassNames(LumoUtility.Margin.Top.XLARGE, LumoUtility.Margin.Bottom.MEDIUM);
-        add(header);
-        add(new Paragraph("It’s a place where you can grow your own UI 🤗"));
+        UnorderedList unorderedList = new UnorderedList();
+        ListItem item0 = new ListItem("Simple to set up - just initialize your Music library and begin listening.");
+        ListItem item1 = new ListItem("Easy to understand - minimal GUI interface allows for stream-lined access to your library.");
+        ListItem item2 = new ListItem("Manage your library by importing more tracks or deleting them.");
 
-        setSizeFull();
-        setJustifyContentMode(FlexComponent.JustifyContentMode.CENTER);
-        setDefaultHorizontalComponentAlignment(FlexComponent.Alignment.CENTER);
-        getStyle().set("text-align", "center");
+        unorderedList.add(item0, item1, item2);
+
+        add(h2Features, pFeature, unorderedList);
     }
 
 }
